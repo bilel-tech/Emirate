93e59f85c23f4701849a27aef82886fe757ad0f2
MonsterComputer <MonsterComputer@DESKTOP-HP6TDQG>
2019-12-28 19:16:56 +0100

    Add project files.

---
+++ EmirateHMBot/MainForm.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Controls;
+using MetroFramework.Forms;
+using Newtonsoft.Json.Linq;
+using EmirateHMBot.Models;
+using OfficeOpenXml;
+
+namespace EmirateHMBot
+{
+    public partial class MainForm : MetroForm
+    {
+        public bool LogToUi = true;
+        public bool LogToFile = true;
+        ExcelPackage _package;
+        ExcelWorksheet _worksheet;
+        int _r;
+        Random rnd = new Random();
+        private readonly string _path = Application.StartupPath;
+        private int _nbr;
+        private int _total;
+        private int _maxConcurrency;
+        public HttpCaller HttpCaller = new HttpCaller();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        int delayMin, delayMax;
+        string user, pass;
+
+        private async Task MainWork()
+        {
+            await Task.Delay(3000);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ServicePointManager.DefaultConnectionLimit = 65000;
+            Directory.CreateDirectory("data");
+            Application.ThreadException += Application_ThreadException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            Utility.CreateDb();
+            Utility.LoadConfig();
+            inputI.Text = _path + @"\input.xlsx";
+            outputI.Text = _path + @"\output.xlsx";
+            Utility.InitCntrl(this);
+        }
+        static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.ToString(), @"Unhandled Thread Exception");
+        }
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show((e.ExceptionObject as Exception)?.ToString(), @"Unhandled UI Exception");
+        }
+        #region UIFunctions
+        public delegate void WriteToLogD(string s, Color c);
+        public void WriteToLog(string s, Color c)
+        {
+            try
+            {
+                if (InvokeRequired)
+                {
+                    Invoke(new WriteToLogD(WriteToLog), s, c);
+                    return;
+                }
+                if (LogToUi)
+                {
+                    if (DebugT.Lines.Length > 5000)
+                    {
+                        DebugT.Text = "";
+                    }
+                    DebugT.SelectionStart = DebugT.Text.Length;
+                    DebugT.SelectionColor = c;
+                    DebugT.AppendText(DateTime.Now.ToString(Utility.SimpleDateFormat) + " : " + s + Environment.NewLine);
+                }
+                Console.WriteLine(DateTime.Now.ToString(Utility.SimpleDateFormat) + @" : " + s);
+                if (LogToFile)
+                {
+                    File.AppendAllText(_path + "/data/log.txt", DateTime.Now.ToString(Utility.SimpleDateFormat) + @" : " + s + Environment.NewLine);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        public void NormalLog(string s)
+        {
+            WriteToLog(s, Color.Black);
+        }
+        public void ErrorLog(string s)
+        {
+            WriteToLog(s, Color.Red);
+        }
+        public void SuccessLog(string s)
+        {
+            WriteToLog(s, Color.Green);
+        }
+        public void CommandLog(string s)
+        {
+            WriteToLog(s, Color.Blue);
+        }
+
+        public delegate void SetProgressD(int x);
+        public void SetProgress(int x)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new SetProgressD(SetProgress), x);
+                return;
+            }
+            if ((x <= 100))
+            {
+                ProgressB.Value = x;
+            }
+        }
+        public delegate void DisplayD(string s);
+        public void Display(string s)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new DisplayD(Display), s);
+                return;
+            }
+            displayT.Text = s;
+        }
+
+        #endregion
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Utility.Config = new Dictionary<string, string>();
+            Utility.SaveCntrl(this);
+            Utility.SaveConfig();
+        }
+        private async void startB_Click(object sender, EventArgs e)
+        {
+            LogToUi = logToUII.Checked;
+            LogToFile = logToFileI.Checked;
+            _maxConcurrency = (int)delayMinI.Value;
+            //we spin it in a new worker thread
+            //Task.Run(MainWork);
+            //we run mainWork on the UI thread
+            await MainWork();
+
+        }
+        private void loadInputB_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog o = new OpenFileDialog { Filter = @"xlsx|*.xlsx", InitialDirectory = _path };
+            if (o.ShowDialog() == DialogResult.OK)
+            {
+                inputI.Text = o.FileName;
+            }
+        }
+        private void openInputB_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start(inputI.Text);
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(ex.ToString());
+            }
+        }
+        private void openOutputB_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start(outputI.Text);
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(ex.ToString());
+            }
+        }
+        private void loadOutputB_Click_1(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog
+            {
+                Filter = @"xlsx file|*.xlsx",
+                Title = @"Select the output location"
+            };
+            saveFileDialog1.ShowDialog();
+            if (saveFileDialog1.FileName != "")
+            {
+                outputI.Text = saveFileDialog1.FileName;
+            }
+        }
+
+        async Task<string> Login()
+        {
+            Display("logging in to www.merchantwords.com...");
+            List<KeyValuePair<string, string>> formData = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("email",user),
+                new KeyValuePair<string, string>("password",pass),
+            };
+            var response = await HttpCaller.PostFormData("https://www.merchantwords.com/login", formData);
+            if (response.error != null)
+            {
+                return response.error;
+            }
+            if (response.html.Contains("Email and passwords don't match"))
+            {
+                return ("Email and passwords don't match");
+            }
+            return null;//we are cool
+        }
+
+        private void startB_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
